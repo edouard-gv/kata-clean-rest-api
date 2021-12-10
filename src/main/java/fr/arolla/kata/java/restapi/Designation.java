6ff01e3b60c1c649b3abfefc9b4bbb89dbd9424e
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2021-12-10 21:42:46 +0100

    Mais je peux quand même changer les noms d'attribut dans mon constructeur en le précisant à Jackson. L'infra déborde méchamment dans le domaine, mais au moins ce débordement est explicite (ça pique de faire planter une API juste parce qu'on a renommé les paramètre d'un constructeur...).

--- src/main/java/fr/arolla/kata/java/restapi/Designation.java
+++ src/main/java/fr/arolla/kata/java/restapi/Designation.java
@@ -1,5 +1,8 @@
 package fr.arolla.kata.java.restapi;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Designation {
     public String getDenominationUsuelle() {
         return safeString(denominationUsuelle);
@@ -16,8 +19,9 @@ public class Designation {
     private final String denominationUsuelle;
     private final String enseigne;
 
-    public Designation(String denomination_usuelle, String enseigne) {
-        this.denominationUsuelle = clean(denomination_usuelle);
+    @JsonCreator
+    public Designation(@JsonProperty("denomination_usuelle") String denominationUsuelle, String enseigne) {
+        this.denominationUsuelle = clean(denominationUsuelle);
         this.enseigne = clean(enseigne);
     }
 
